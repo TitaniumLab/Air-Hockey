aa4376496b76a3e97dbbbfbf8c4aa9b77d8cf3fe
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-12-26 20:55:43 +0300

    Work in progress

--- Assets/Project resources/_Scripts/DistributionOfPlayers.cs
+++ Assets/Project resources/_Scripts/DistributionOfPlayers.cs
@@ -7,12 +7,20 @@ namespace AirHockey
     public class DistributionOfPlayers : NetworkBehaviour
     {
         [SerializeField] private Camera[] _cameras;
+        [SerializeField] private NetworkObject _networkObject;
         [SerializeField] private MalletController[] _mallets;
         public static DistributionOfPlayers Instance;
 
         private void Awake()
         {
             Instance = this;
+            //var netobj = Instantiate(_networkObject);
+            //_mallets[0] = netobj.GetComponent<MalletController>();
+            ////netobj.Spawn();
+            //var netobj2 = Instantiate(_networkObject);
+            //_mallets[1] = netobj2.GetComponent<MalletController>();
+            //netobj2.Spawn();
+
             //NetworkManager.SceneManager.OnSceneEvent += OnSceneLoad;
         }
 
